f1d0b3b3567cef8b667caea7ec8144669bd76809
rohitkpatel7 <rkpatel07082003@gmail.com>
2024-06-08 09:05:45 +0000

    data types

---
+++ 0_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3)  //we are using node js not brouser
+
+//code readability should be high
+
+/*
+java script original document ecma script  
+https://tc39.es/ecma262/
+*/
+
+let name="JNFOWPGP"
+let age=95
+let isLoggedIn=false
+
+//number=>2 to power 53
+//bigint
+//string=""
+//boolean=>true/false
+//null=>standalone value
+//undefined=>value not assigned
+//symbol=>unique
+
+//object
+
+console.log(typeof "fweq");//string
+console.log(typeof null);//object
+console.log(typeof undefined);//undefind
+console.log(typeof 0/0);//NaN
+
+
