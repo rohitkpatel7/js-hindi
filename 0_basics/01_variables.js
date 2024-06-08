bd924639fe4b1de918f4a5958d2e3747c6f3a746
rohitkpatel7 <rkpatel07082003@gmail.com>
2024-06-08 14:07:33 +0530

    Create 01_variables.js
    just see about variables and constant

---
+++ 0_basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId=144543
+let accountEmail="rohitp3@gfg.com"
+var accountPassword="24562"
+accountCity="jaipur"
+let accountState;
+/*
+prefer not to use var
+because of issue in block scope 
+and functionam scope
+
+*/
+
+
+
+//not allowed
+//accountId=534
+ accountEmail="edgds@dfwf.com"
+ accountPassword="345621"
+ accountCity="srikakulam"
+
+//console.log(accountId);
+
+console.table([ accountId,accountEmail,accountPassword,accountCity,accountState]);
