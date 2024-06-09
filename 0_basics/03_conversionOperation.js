63eedec252c84e0a4e2688fc41e26429be33b128
rohitkpatel7 <rkpatel07082003@gmail.com>
2024-06-09 09:21:24 +0000

    conversion of data type

---
+++ 0_basics/03_conversionOperation.js
@@ -0,0 +1,92 @@
+//"33"=>33
+
+// let score="33"
+// //const {score}=req.body
+// console.log(typeof(score));
+// console.log(typeof score);
+
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(typeof score);
+// console.log(valueInNumber);
+
+
+//"33abc"=>NaN
+
+// let newScore2="33abc"
+// let valueInNumber2=Number(newScore2)
+// console.log(typeof newScore);
+// console.log(typeof valueInNumber2);
+// console.log(newScore2);
+// console.log(valueInNumber2)
+
+
+//null=>0
+
+// let newScore3=null
+// let valueInNumber3=Number(newScore3)
+// console.log(typeof newScore3);
+// console.log(typeof valueInNumber3);
+// console.log(newScore3);
+// console.log(valueInNumber3);
+
+
+
+// let newScore4=undefined
+// let valueInNumber4=Number(newScore4)
+// console.log(typeof newScore4);
+// console.log(typeof valueInNumber4);
+// console.log(newScore4);
+// console.log(valueInNumber4);
+
+// //true=>1
+// //false=>0
+
+// let newScore5=true
+// let valueInNumber5=Number(newScore5)
+// console.log(typeof newScore5);
+// console.log(typeof valueInNumber5);
+// console.log(newScore5);
+// console.log(valueInNumber5);
+
+
+// let newScore6="string"
+// let valueInNumber6=Number(newScore6)
+// console.log(typeof newScore6);
+// console.log(typeof valueInNumber6);
+// console.log(newScore6);
+// console.log(valueInNumber6);
+
+
+// let isLoggedIn=1
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(typeof isLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+// console.log(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+
+//""=>false " "=>true "nfks"=>true
+
+// let str=""
+// let boolStr=Boolean(str)
+// console.log(boolStr);
+
+// let str2=" "
+// let boolStr2=Boolean(str2)
+// console.log(boolStr2);
+
+// let str3="string"
+// let boolStr3=Boolean(str3)
+// console.log(boolStr3);
+
+
+let someNumber=33
+let stringNumber=String(someNumber)
+//console.log(typeof someNumber);
+console.log(typeof stringNumber);
+//console.log(someNumber);
+console.log(stringNumber);
+
+
+// node 0_basics/03_conversionOperation.js 
