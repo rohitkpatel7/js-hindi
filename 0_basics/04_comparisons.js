9f656ea8b5bdea998bf3bc3fb15ef29896025848
rohitkpatel7 <rkpatel07082003@gmail.com>
2024-06-09 10:17:25 +0000

    comparision of data and datatypes

---
+++ 0_basics/04_comparisons.js
@@ -0,0 +1,38 @@
+
+// console.log(2>1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+//comparision in diff data type 
+// console.log("2">1);
+// console.log("02">1);
+
+
+// console.log(null==0);
+// console.log(null>=0);
+// console.log(null<=0);
+// console.log(null>0);
+// console.log(null<0);
+
+//equality check== and comparision ><>=<= work differently
+//comparision convert null toa number treating it as 0. that's why  null>=0 is true and null>0 is false
+
+
+//for undefined => always give false
+
+// console.log(undefined==0);
+// console.log(undefined>=0);
+// console.log(undefined<=0);
+// console.log(undefined>0);
+// console.log(undefined<0);
+
+
+//strict check (===) :check data type also
+console.log("2"===2)  //=>false
+
+
+
+
+
+
