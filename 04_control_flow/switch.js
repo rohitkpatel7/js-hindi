98bab6691720645c565c00e7a9b1b9fdb867f7d5
rohitkpatel7 <rkpatel07082003@gmail.com>
2024-06-16 12:01:17 +0000

    switch statement

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,53 @@
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 3;
+// switch (month) {
+//   case 1:
+//     console.log("january");
+//     break;
+//   case 2:
+//     console.log("Feb");
+//     break;
+//   case 3:
+//     console.log("March");
+//     break;
+//   case 4:
+//     console.log("April");
+//     break;
+
+//   default:
+//     console.log("default case match");
+//     break;
+// }
+
+
+
+// const month = "feb";
+// switch (month) {
+//   case "jan":
+//     console.log("january");
+//     break;
+//   case "feb":
+//     console.log("Feb");
+//     break;
+//   case "march":
+//     console.log("March");
+//     break;
+//   case "apr":
+//     console.log("April");
+//     break;
+
+//   default:
+//     console.log("default case match");
+//     break;
+// }
+
+
+
