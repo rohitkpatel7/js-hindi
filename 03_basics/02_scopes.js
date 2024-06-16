a05a513fb0bf73bb343ff2cdf69c64366ce8ed40
rohitkpatel7 <rkpatel07082003@gmail.com>
2024-06-16 08:35:27 +0000

    scope and variable

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,48 @@
+//var c=200
+
+let a = 300;
+if (true) {
+  let a = 10;
+  const b = 20;
+  //console.log("inner:", a);
+}
+//console.log(a);
+//  console.log(b);
+//  console.log(c);
+
+function one() {
+  const username = "rohit";
+
+  function two() {
+    const website = "youtube";
+    console.log(username);
+  }
+  // console.log(website);
+
+  two();
+}
+
+//one()
+
+if (true) {
+  const username = "rohit";
+  if (username === "rohit") {
+    const website = " youtube";
+    //console.log(username + website);
+  }
+  //console.log(website);
+}
+//console.log(username);
+
+// ++++++++++ interesting +++++++++++
+
+console.log(addone(5));
+function addone(num) {
+  return num + 1;
+}
+
+
+console.log(addTwo(5));
+const addTwo = function (num) {
+  return num + 2;
+};
