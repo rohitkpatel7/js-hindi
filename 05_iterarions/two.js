504532f080dac91eda1d563effe290a227a9256a
rohitkpatel7 <rkpatel07082003@gmail.com>
2024-06-17 06:11:07 +0000

    do while

---
+++ 05_iterarions/two.js
@@ -0,0 +1,28 @@
+//while
+let index = 0;
+while (index <= 10) {
+  //console.log(`value of index is ${index}`);
+  index += 2;
+}
+
+let myArray = ["flash", "batman", "superman"];
+let arr = 0;
+while (arr < myArray.length) {
+  //console.log(`value is ${myArray[arr]}`);
+  arr = arr + 1;
+}
+
+let score1=1
+do{
+//console.log(`score is ${score1}`);
+score++
+}while(score1<=10);
+
+
+// let score=12
+// do{
+// console.log(`score is ${score}`);
+// score++
+// }while(score<=10);
+
+
