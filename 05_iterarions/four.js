ffb490cacbb2603cb26a85a33070d07f46abd93b
rohitkpatel7 <rkpatel07082003@gmail.com>
2024-06-17 07:39:42 +0000

    for in loop

---
+++ 05_iterarions/four.js
@@ -0,0 +1,32 @@
+
+// const myObject={
+//     js: "JavaScript",
+//     cpp:"C++",
+//     rb: "ruby",
+//     swift:"swift by apple"
+// }
+
+// for (const key in myObject) {
+//     //console.log(myObject[key]);
+//     console.log(`${key} is used for ${myObject[key]}`);
+// }
+
+// const programming=["js","rb","java","cpp"]
+
+// for (const key in programming) {
+// console.log(programming[key]);
+// }
+
+// const map = new Map();
+// map.set("IN", "India");
+// map.set("uk", "United Kingdom");
+// map.set("Fr", "France");
+// map.set("IN", "India");
+// for (const key in map) {
+//     console.log(key);
+// }
+
+
+
+
+
